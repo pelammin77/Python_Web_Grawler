d7d4b45324a6483b49a5f45b19e74b7d41406d6d
LAMMINAHO\Petri <lammpe77@gmail.com>
2017-05-25 00:33:05 +0300

    Link_finder class: ready Create Spider class file

--- Grawler_general.py
+++ Grawler_general.py
@@ -1,16 +1,69 @@
 """
-     Grawler.py 
-      Simple Web grawler 
-      autthor: Petri Lamminaho
+File: Grawler.py 
+Simple Web grawler "General functions" 
+Author: Petri Lamminaho
+File stared 24th of  May 2017 
+
 """
 import os
 
-# project is website your grawling
+
+# Create new project.
+# Project is website your crawling
 def create_projet_directory(directory):
     if not os.path.exists(directory):
-        print("Creating directory "+ directory)
+        print("Creating directory " + directory)
         os.makedirs(directory)
 
 
-create_projet_directory("jyu")
-create_projet_directory("mtv3")
+# Create new data file
+def create_data_files(project_name, base_url):
+    waiting_list_page = project_name + '/queue.txt'
+    completed_page = project_name + '/crawled.txt'
+    if not os.path.isfile(waiting_list_page):
+        print("Creating new data files")
+        write_file(waiting_list_page, base_url)
+        write_file(completed_page, '')
+
+
+# write data to file
+def write_file(path, data):
+    f = open(path, 'w')
+    f.write(data)
+    f.close()
+
+
+# Add new line(link) to file
+def append_data_to_file(path, data):
+    with open(path, 'a') as file:
+        file.write(data + '\n')
+
+
+# create new empty file same name that old file
+def empty_file_content(path):
+    with open(path, 'w'):
+        pass
+
+
+# save file contents to set
+def file_to_set(file_name):
+    result = set()
+    with open(file_name, 'rt') as f:
+        for line in f:
+            result.add(line.replace('\n', ' '))
+    return result
+
+
+# convert and save set to file
+def set_to_file(links, file):
+    empty_file_content(file)
+    for link in sorted(links):
+        append_data_to_file(file, link)
+
+
+
+
+# test
+# create_projet_directory("jyu")
+# create_projet_directory("mtv3")
+# create_data_files('jyu', 'jyu.fi')
---
+++ link_finder.py
@@ -0,0 +1,40 @@
+"""
+File: LinkFinder.py 
+Author: Petri Lamminaho 
+Simple class that returns links from given web page
+Made 24-25 May 2017
+
+
+"""
+
+from html.parser import HTMLParser
+from urllib import parse
+
+
+class LinkFinder(HTMLParser):
+    def __init__(self, base_url, page_url):
+        super().__init__()
+        self. __base_url = base_url
+        self. __page_url = page_url
+        self. __links = set()
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'a':
+            for (attribute, value)in attrs:
+                if attribute == 'href':
+                    url = parse.urljoin(self. __base_url, value)
+                    self. __links.add(url)
+
+    def get_links(self):
+        return self. __links
+
+    def error(self, message):
+        pass
+
+
+
+
+#test
+finder = LinkFinder()
+finder.feed('<html> <head> <title> Testing </title> </head>'
+           '<body> <h1> Parse page </h1> </body> </html>')
